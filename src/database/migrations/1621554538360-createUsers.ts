2a33fc470eeb06fdab4b446d0949f3da0fb78374
Luiz Rioja <lulyrioja@gmail.com>
2021-05-20 21:25:45 -0300

    :alien: Creating user migration

---
+++ src/database/migrations/1621554538360-createUsers.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createUsers1621554538360 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "integer",
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "gender",
+            type: "varchar",
+          },
+          {
+            name: "address",
+            type: "varchar",
+          },
+          {
+            name: "birthDate",
+            type: "date",
+          },
+          {
+            name: "telephone",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
