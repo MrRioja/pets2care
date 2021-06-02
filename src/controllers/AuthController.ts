3f6a4fd06f9dacc73f03f782bbb62e1e1bc64c06
Luiz Rioja <lulyrioja@gmail.com>
2021-06-02 00:13:32 -0300

    :alien: Creating the password recovery route

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,11 +1,13 @@
 import bcrypt from "bcrypt";
 import AppError from "../errors/AppError";
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
+import { getConnection, getCustomRepository } from "typeorm";
 import { generateToken } from "../config/generateToken";
 import { UsersRepository } from "../repositories/UsersRepository";
 import users_view from "../views/users_view";
+import User from "../models/User";
 const crypto = require("crypto");
+const mailer = require("../modules/mailer");
 
 class AuthController {
   async create(req: Request, res: Response) {
@@ -45,6 +47,37 @@ class AuthController {
           .status(400)
           .json({ message: new AppError("User not found").message });
       }
+
+      const token = crypto.randomBytes(20).toString("hex");
+
+      const now = new Date();
+      now.setHours(now.getHours() + 1);
+
+      await getConnection()
+        .createQueryBuilder()
+        .update(User)
+        .set({ passwordResetToken: token, passwordResetExpires: now })
+        .where("id = :id", { id: user.id })
+        .execute();
+
+      mailer.sendMail(
+        {
+          to: email,
+          from: "suporte@pets2care.com",
+          template: "auth/forgot_password",
+          context: { token },
+        },
+        (error: Error) => {
+          if (error) {
+            console.log(error);
+
+            res
+              .status(400)
+              .send({ error: "Cannot send forgot password email" });
+          }
+          return res.send();
+        }
+      );
     } catch (error) {
       res.status(400).json({ error: "Erro on forgot password, try again" });
     }
