81d42b734cfe2604244674c3ba090b929e414e6c
Luiz Rioja <lulyrioja@gmail.com>
2021-06-02 00:11:06 -0300

    :alien: Adding the AppError to the App

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import path from "path";
 import cors from "cors";
+import AppError from "./errors/AppError";
 
 import "./database/connection";
 import errorHandler from "./errors/handler";
@@ -9,7 +10,7 @@ import errorHandler from "./errors/handler";
 import routes from "./routes";
 
 import * as dotenv from "dotenv";
-dotenv.config({ path: __dirname+'/.env' });
+dotenv.config({ path: __dirname + "/.env" });
 
 const app = express();
 
@@ -18,5 +19,18 @@ app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use(errorHandler);
+
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "Error",
+    message: `Internal server error ${err.message}`,
+  });
+});
 
 app.listen(process.env.PORT || 3333);
