fbedf27f528926156077db1e8180a8dc59687046
Luiz Rioja <lulyrioja@gmail.com>
2021-06-04 23:49:09 -0300

    :alien: Adding stringToBoolean function

--- src/utils/stringToBoolean.ts
+++ src/utils/stringToBoolean.ts
@@ -1,8 +1,12 @@
-export default async function stringToBoolean(vector: String[]) {
+export default async function arrayStringToBoolean(vector: String[]) {
   var vectorBool: boolean[] = [];
 
   vector.forEach((element) => {
     vectorBool.push(Boolean(element.toLowerCase() == "true"));
   });
   return vectorBool;
+}
+
+export async function stringToBoolean(string: String) {
+  return Boolean(string.toLowerCase() == "true");
 }
