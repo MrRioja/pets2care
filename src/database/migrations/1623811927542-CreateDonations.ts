326c3f62de2c6e344c8446d1176c67c439ded02a
Luiz Rioja <lulyrioja@gmail.com>
2021-06-15 23:54:08 -0300

    :alien: Creating files for the donation stream

---
+++ src/database/migrations/1623811927542-CreateDonations.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateDonations1623811927542 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
